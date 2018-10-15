911b64d066fba7ed1a1ba23d7d8a8f2d2e8b219b
Daniel <dan_rigg11@hotmail.com>
2018-10-15 16:15:41 +0100

    first commit

---
+++ EAFG_Website/resources/views/index.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="{{asset('css/master.css')}}">
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+
+
+</head>
+<body>
+
+  <!--Header-->
+  <section>
+    <div class="sectionHeader">
+      <div class="container">
+        <h5 class="float-right m-auto pt-1">enquiries@eastanglianfasciaandguttering.com</h5>
+        <h3 class="ml-3 mb-0">01603 443830</h3>
+      </div>
+    </div>
+  </section>
+
+  <!--Navbar-->
+  <section class="sticky-top">
+    <div class="sectionNavbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <h2 class="ml-3 mb-0">EAFG</h2>
+            <p class="pl-3 mb-0 titletext">East Anglia Fascia & Guttering Ltd</p>
+          </div>
+          <div class="col-9 m-auto pt-2 pr-2 pl-0">
+            <div class="row">
+              <div class="col p-0 text-center">
+                <a href="#">Home</a>
+              </div>
+              <div class="col p-0 text-center">
+                <a href="#">Our Services</a>
+              </div>
+              <div class="col p-0 text-center">
+                <a href="#">Project Gallery</a>
+              </div>
+              <div class="col p-0 text-center">
+                <a href="#">Composite Doors</a>
+              </div>
+              <div class="col p-0 text-center">
+                <a href="#">Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--Slidebar-->
+  <section>
+    <div class="sectionSlidebar">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner sliderh">
+          <div class="carousel-item active">
+            <img class="d-block w-100 img-h" src="{{asset('images/slide_gutter.jpg')}}" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100 img-h" src="{{asset('images/slide_fascia_boards.png')}}" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100 img-h" src="{{asset('images/slide_waterandwaste.png')}}" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!--ScrollBar-->
+  <section class="sectionScrollBar">
+    <div class="">
+      <div class="container">
+        <h3>Scroll down for more information on East Anglia Fascia & Guttering Ltd</h3>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <img class="img-fluid onHover" src="{{asset('images/img_fascia.png')}}">
+        </div>
+        <div class="col-3">
+          <img class="img-fluid onHover" src="{{asset('images/img_repair.png')}}">
+        </div>
+        <div class="col-3">
+          <img class="img-fluid onHover" src="{{asset('images/img_door.png')}}">
+        </div>
+        <div class="col-3">
+          <img class="img-fluid onHover" src="{{asset('images/img_request.png')}}">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <hr style="width:50%; height:2px; border-top: 2px solid #cf1f2e;">
+          <h4 class="text-center">Replacing</h4>
+        </div>
+        <div class="col-3">
+          <hr style="width:50%; height:2px; border-top: 2px solid #cf1f2e;">
+          <h4 class="text-center">Repairing</h4>
+        </div>
+        <div class="col-3">
+          <hr style="width:50%; height:2px; border-top: 2px solid #cf1f2e;">
+          <h4 class="text-center">Doors</h4>
+        </div>
+        <div class="col-3">
+          <hr style="width:50%; height:2px; border-top: 2px solid #cf1f2e;">
+          <h4 class="text-center">Online Quote</h4>
+        </div>
+      </div>
+      <div style="margin-top:20px" class="">
+        <h2>Individualized Roofing and Roofline Options</h2>
+        <p>At East Anglian Fascia and Guttering ltd We will provide you with a full consultation to explain all of your available options so that you can choose the roofing or roofline plan that’s right for your home or business. We will work with you and adhere to your personal needs. 99% of the time you will deal with one person from start to finish!  That person will visit, quote, meet and fit your requirements! This cuts out the need for expensive commission based sales people.</p>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <hr style="height:2px; border-top: 2px solid #cf1f2e;">
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img style="height:150px" class="d-block w-100" src="{{asset('images/img_whiteout.jpg')}}" alt="First slide">
+      <div class="carousel-caption pt-1">
+    <h5 class="text-dark text-left">Mr White, Norwich</h5>
+    <p class="text-dark font-italic text-left">Excellent job, total satisfaction. The attention to details by your installer was exceptional.</p>
+  </div>
+    </div>
+
+    <div class="carousel-item">
+      <img style="height:150px" class="d-block w-100" src="{{asset('images/img_whiteout.jpg')}}" alt="Second slide">
+      <div class="carousel-caption pt-1">
+        <h5 class="text-dark text-left">Mr S Balaam, Norfolk</h5>
+        <p class="text-dark font-italic text-left">I would like to express how impressed I was with the total process, from beginning to end, of my dealings with your company. I was particularly impressed with the manner in which all of those involved communicated with each other and coordinated the work for a seamless and very efficient completion. I am also very grateful to Steve Cutler, your designated builder, who outlined his plans clearly and executed it to a very high standard. I shall </p>
+  </div>
+    </div>
+
+    <div class="carousel-item">
+      <img style="height:150px" class="d-block w-100" src="{{asset('images/img_whiteout.jpg')}}" alt="Third slide">
+      <div class="carousel-caption pt-1">
+        <h5 class="text-dark text-left">Mr & Mrs Ogden, Norfolk</h5>
+        <p class="text-dark font-italic text-left">My wife and myself were impressed by the standard of workmanship, in particular the care with which your employees, Gary and Wayne, went about their tasks when newly windowing our house. Who said the British artisan had lost his measure! Our best wishes to your diligent workforce and to you as a company. Finn, our dog, was equally impressed. You could tell by his joyous bark and the saluting of his tail whenever Wayne and Gary appeared. Thanks to you all! P.S. Please pass on our best wishes to two fine workmen. You are fortunate to have them. May they have the opportunity to pass on their skills to a younger generation!</p>
+  </div>
+    </div>
+  </div>
+</div>
+      <hr style="height:2px; border-top: 2px solid #cf1f2e;">
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
