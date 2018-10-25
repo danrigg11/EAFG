ca455ea205be41460f3d6969ee93b5e59dfdbf87
Daniel <dan_rigg11@hotmail.com>
2018-10-25 15:04:53 +0100

    finished services cateloge

--- EAFG_Website/resources/views/services.blade.php
+++ EAFG_Website/resources/views/services.blade.php
@@ -58,13 +58,21 @@
     imgText3.innerHTML = imgs.alt;
     expandImg3.parentElement.style.display = "block";
   }
+
+  function myFunction4(imgs) {
+    var expandImg4 = document.getElementById("expandedImg4");
+    var imgText4 = document.getElementById("imgtext4");
+    expandImg4.src = imgs.src;
+    imgText4.innerHTML = imgs.alt;
+    expandImg4.parentElement.style.display = "block";
+  }
 </script>
 
 <div class="container-fluid">
   <h1 class="text-center display-1">Services</h1>
   <!--Back Section-->
   <div id="backSection" style="display: none;">
-    <button type="button" onclick="backOptionFunction()" class="">Back</button>
+    <button type="button" onclick="backOptionFunction()" class="btn btn-primary ml-3">Back</button>
   </div>
 
   <!--Menu Tabs-->
@@ -73,11 +81,37 @@
 
       <div class="col-xs-12 col-sm-6 col-md-4">
         <div class="card" style="width: 18rem; margin-bottom:30px; width:100%">
-          <img class="card-img-top" src="{{asset('images/img_roofline_2.jpg')}}" alt="Card image cap">
+          <img class="card-img-top" src="{{asset('images/img_roofline_menu_2.jpg')}}" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">Roofline</h5>
             <p class="card-text">Ideal for both new homes and replacement projects.</p>
-            <button type="button" onclick="rooflineOptionFunction()" class="">View cateloge</button>
+            <div class="d-flex justify-content-between">
+              <button type="button" onclick="rooflineOptionFunction()" class="btn btn-primary">View</button>
+              <!-- Button trigger modal -->
+              <button type="button" class="btn btn-warning" data-toggle="modal" data-target=".bd-example-modal-lg">
+                Roofline Explained
+              </button>
+
+              <!-- Modal -->
+              <div class="modal fade bd-example-modal-lg" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLongTitle">Roofline Explained</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <img src="{{asset('images/roofline-explained.jpg')}}" alt="roofline Drawing" class="img-fluid">
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -88,7 +122,7 @@
           <div class="card-body">
             <h5 class="card-title">Roofline Repair</h5>
             <p class="card-text">Ideal for both new homes and replacement projects.</p>
-            <button type="button" onclick="rooflineRepairOptionFunction()" class="">View cateloge</button>
+            <button type="button" onclick="rooflineRepairOptionFunction()" class="btn btn-primary">View</button>
           </div>
         </div>
       </div>
@@ -99,16 +133,17 @@
           <div class="card-body">
             <h5 class="card-title">EPDM Roofs</h5>
             <p class="card-text">Ideal for both new homes and replacement projects.</p>
-            <button type="button" onclick="epdmOptionFunction()" class="">View cateloge</button>
+            <button type="button" onclick="epdmOptionFunction()" class="btn btn-primary">View</button>
           </div>
         </div>
       </div>
 
+
     </div>
   </div>
-  </div>
+</div>
 
-  <div class="container">
+<div class="container">
   <!--Roofline Section-->
   <div id="rooflineSection" style="display: none;">
     <div class="row">
@@ -167,12 +202,12 @@
 
 
 
-      <!--Hollow Soffit-->
+      <!--Soffit Board-->
       <div class="col-sm-12 col-md-6" style="padding-right:40px;">
         <hr style="width:100%; height:2px; border-top: 2px solid #cf1f2e;">
         <div class="text-center">
           <div class="container2">
-            <h2>Hollow Soffit</h2>
+            <h2>Soffit Board</h2>
             <img id="expandedImg2" style="width:50%" src="{{asset('images/soffit_Board_Anthracite_Grey.jpg')}}" >
             <div id="imgtext2">Anthracite Grey</div>
           </div>
@@ -210,12 +245,12 @@
 
 
 
-      <!--Single Sided Cladding-->
+      <!--Cladding-->
       <div class="col-sm-12 col-md-6" style="padding-right:40px;">
         <hr style="width:100%; height:2px; border-top: 2px solid #cf1f2e;">
         <div class="text-center">
           <div class="container3">
-            <h2>Single Sided Cladding</h2>
+            <h2>Cladding</h2>
             <img id="expandedImg3" style="width:50%" src="{{asset('images/Shiplap-cladding_White.jpg')}}" >
             <div id="imgtext3">White</div>
           </div>
@@ -237,20 +272,83 @@
       </div>
 
 
+      <!--Guttering-->
+      <div class="col-sm-12 col-md-6" style="padding-right:40px;">
+        <hr style="width:100%; height:2px; border-top: 2px solid #cf1f2e;">
+        <div class="text-center">
+          <div class="container4">
+            <h2>Guttering</h2>
+            <img id="expandedImg4" style="width:50%" src="{{asset('images/GutteringDeepFlow.jpg')}}" >
+            <div id="imgtext4">Deep Flow</div>
+          </div>
+        </div>
+
+        <div class="col m-auto">
+          <div class="row">
+            <div class="col-3 text-center" style="padding:0px">
+              <img src="{{asset('images/GutteringDeepFlow.jpg')}}" alt="Deep Flow" style="width:100%" onclick="myFunction4(this);">
+            </div>
+            <div class="col-3 text-center" style="padding:0px">
+              <img src="{{asset('images/GutteringHalfRound.jpeg')}}" alt="Half Round" style="width:100%" onclick="myFunction4(this);">
+            </div>
+            <div class="col-3 text-center" style="padding:0px">
+              <img src="{{asset('images/GutteringOgee.jpg')}}" alt="Ogee" style="width:100%" onclick="myFunction4(this);">
+            </div>
+            <div class="col-3 text-center" style="padding:0px">
+              <img src="{{asset('images/GutteringSquare.jpeg')}}" alt="Square" style="width:100%" onclick="myFunction4(this);">
+            </div>
+          </div>
+        </div>
       </div>
 
+
+    </div>
+
+  </div>
+</div>
+
+<!--Roofline Repair Section-->
+<div id="rooflineRepairSection" style="display: none;">
+  <div class="container">
+    <h1>Roofline Repair</h1>
+    <p>If you live in East Anglia and are looking to repair, replace or upgrade your existing roofline, our team of professional installers with <span style="font-family: numberfont, serif !important;">30+</span> years experience are here to help. Not only will a new roofline from EAFG increase the weatherproofing of your home, our long lasting materials require very little maintenance, a roofline from EAFG is a possitive investment for your property.</p>
+    <p>We can install everything from uPVC fascias and soffits to guttering and rainwater systems, helping your roofline run properly.</p>
+    </div>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="{{asset('images/img_roofline_2.jpg')}}" alt="First slide roofline"style="background-size:cover">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{asset('images/img_roofline.jpg')}}" alt="Second slide roofline"style="background-size:cover;">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="{{asset('images/img_roofline_3.jpg')}}" alt="Third slide"style="background-size:cover">
     </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
-  <!--Roofline Repair Section-->
-  <div id="rooflineRepairSection" style="display: none;">
-    <h1>Roofline Repair</h1>
+
   </div>
 
-  <!--EPDM Section-->
-  <div id="epdmSection" style="display: none;">
-    <h1>EPDM</h1>
-  </div>
+<!--EPDM Section-->
+<div id="epdmSection" style="display: none;">
+  <h1>EPDM</h1>
+</div>
 
 
 
